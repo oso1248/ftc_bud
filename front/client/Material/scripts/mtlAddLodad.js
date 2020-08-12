717f91d270ab72d493378d1a4a723fef560724cf
oso1248 <oso1248@gmail.com>
2020-08-11 20:11:02 -0600

    Initial commit

---
+++ front/client/Material/scripts/mtlAddLodad.js
@@ -0,0 +1,9 @@
+const axios = require('axios').default;
+
+window.addEventListener('load', supplier())
+
+function supplier() {
+  axios.get('http://localhost:5000/api/supplier')
+  console.log('Supplier')
+}
+
