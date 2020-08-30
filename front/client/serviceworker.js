ab8324b71b98b26d88d8e0fec908cb666af852fd
oso1248 <oso1248@gmail.com>
2020-08-30 12:50:40 -0600

    Added Install

---
+++ front/client/serviceworker.js
@@ -0,0 +1,35 @@
+const staticDevCoffee = "budApp-v1"
+const assets = [
+  "/",
+  "./index.html",
+  "./styles/styles.css",
+  "",
+  "./images/bud.png",
+  
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
